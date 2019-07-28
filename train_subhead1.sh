d268117de421f3729b3fa36bd208ddd3d0709095
Xi Yan <yanxi970830@gmail.com>
2019-07-27 20:34:09 -0400

    visualize cluster

---
+++ train_subhead1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+
+python ./code/scripts/cluster/cluster_sobel_twohead.py \
+--model_ind $1  \
+--arch ClusterNet5gTwoHead \
+--mode IID \
+--dataset IMAGENET32 \
+--dataset_root /h/yanxi/Disk/datasets/downsampled-imagenet-32 \
+--gt_k 10 \
+--output_k_A 70 \
+--output_k_B 10 \
+--lamb 1.0 \
+--lr 0.0001  \
+--num_epochs 256 \
+--batch_sz 4000 \
+--num_dataloaders 5 \
+--num_sub_heads 1 \
+--crop_orig \
+--rand_crop_sz 32 \
+--input_sz 32 \
+--head_A_first \
+--head_B_epochs 2 \
+--batchnorm_track \
